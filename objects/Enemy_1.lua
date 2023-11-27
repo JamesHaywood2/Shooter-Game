3420d53df03f4a69c1b4e1b871797741a5866485
Elisabeth Elgin <eje0004@uah.edu>
2023-11-27 14:47:48 -0600

    enemy sprites
    Adding Enemy Sprites

--- objects/Enemy_1.lua
+++ objects/Enemy_1.lua
@@ -3,12 +3,24 @@ local physics = require("physics");
 
 local Square = Enemy:new( {HP=2, fR=720, fT=700, bT=700} );
 
+local opt = 
+{
+  frames = {
+    {x = 15, y = 4, width = 221, height = 118} -- small plane
+  }
+}
+
+local sheet = graphics.newImageSheet("Enemy.png", opt)
+
+
 function Square:spawn()
-  self.shape = display.newRect (display.actualContentWidth+50, math.random(50, display.actualContentHeight-50), 30, 30); 
+  --self.shape = display.newRect (display.actualContentWidth+50, math.random(50, display.actualContentHeight-50), 30, 30); 
+  self.shape = display.newImage(sheet, 1)
   self.shape.pp = self;
   self.shape.tag = "enemy";
   self.shape:setFillColor ( 0, 1, 1);
-  physics.addBody(self.shape, "kinematic"); 
+  outline = graphics.newOutline(1, sheet, 1)
+  physics.addBody(self.shape, "kinematic", {outline = outline}); 
 end
 
 --Will add velocity to the square in the -x direction
