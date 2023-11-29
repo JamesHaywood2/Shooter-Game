4598e061c746a8d44f6a92768b0c9aff74603ba6
Elisabeth Elgin <eje0004@uah.edu>
2023-11-29 02:23:39 -0600

    background music

--- scene/Game.lua
+++ scene/Game.lua
@@ -270,7 +270,6 @@ end
  
 -- "scene:show()"
 function scene:show( event )
- 
    local sceneGroup = self.view
    local phase = event.phase
  
@@ -293,7 +292,7 @@ function scene:show( event )
       -- Called when the scene is now on screen.
       -- Insert code here to make the scene come alive.
       -- Example: start timers, begin animation, play audio, etc.
-
+      audio.play( soundTable["backgroundSound"] );
       gameRunning = true;
       --Start the updater
       Runtime:addEventListener("enterFrame", enterFrame)
